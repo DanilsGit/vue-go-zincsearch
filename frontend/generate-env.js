af2081dbe757da5b5c4550184921d6c5b210541c
danilsgit <danielestebanmarquez12@gmail.com>
2024-07-30 21:19:57 -0500

    fix env creation frontend

--- frontend/generate-env.js
+++ frontend/generate-env.js
@@ -8,7 +8,11 @@ let envContent = '';
 
 envVariables.forEach(variable => {
     console.log(process.env[variable]);
-    envContent += `VITE_${variable}=${process.env[variable]}\n`;
+    if (process.env[variable]) {
+        envContent += `${variable}=${process.env[variable]}\n`;
+    }else {
+        envContent += `VITE_API_URL=http://localhost:8080`;
+    }
 });
 
 writeFileSync('.env', envContent);
